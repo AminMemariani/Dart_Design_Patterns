5e58f8596846e2f14fc36d55876f06e3db000784
Amin Memariani <amin90memar@gmail.com>
2024-10-24 20:43:36 +0330

    added factory code

--- Factory/factory.dart
+++ Factory/factory.dart
@@ -1,3 +1,34 @@
-class Factory {
-  
+abstract class Animal {
+  factory Animal(String type) {
+    switch (type) {
+      case 'dog':
+        return Dog();
+      case 'cat':
+        return Cat();
+      default:
+        throw Exception('Invalid animal type');
+    }
+  }
+
+  void makeSound();
+}
+
+class Dog implements Animal {
+  void makeSound() {
+    print('Woof!');
+  }
+}
+
+class Cat implements Animal {
+  void makeSound() {
+    print('Meow!');
+  }
+}
+
+void main() {
+  Animal animal1 = Animal('dog');
+  animal1.makeSound(); // Output: Woof!
+
+  Animal animal2 = Animal('cat');
+  animal2.makeSound(); // Output: Meow!
 }
